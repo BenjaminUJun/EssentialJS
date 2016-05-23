93ff6fc9d5b4160b9efb5d4824ff4d18c3b9b1d3
Alejandro Gomez <agomez.amdg@gmail.com>
2016-05-23 15:00:41 -0400

    truncate string

--- Beginner/truncate-string.js
+++ Beginner/truncate-string.js
@@ -3,9 +3,13 @@
 // Note that inserting the three dots to the end will add to the string length.
 // However, if the given maximum string length num is less than or equal to 3, then the addition of the three dots does not add to the string length in determining the truncated string.
 
-function truncateString(str, num) {
-  // Clear out that junk in your trunk
-  return str;
+function truncate(str, num) {
+  if(str.length > num){
+      str = str.slice(0,num-3);
+      str += "...";
+    }
+    return str;
+  }
 }
 
 truncateString("A-tisket a-tasket A green and yellow basket", 11);
