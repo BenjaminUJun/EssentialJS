96f8437c0fa2db800b0cba449c93a61b989c52eb
Alejandro Gomez <agomez.amdg@gmail.com>
2016-05-30 14:15:14 -0400

    form validation

--- projects/stock-quote/main.js
+++ projects/stock-quote/main.js
@@ -1,22 +1,36 @@
-$(function(){
+$(function() {
 
-  function init() {
-    console.log('Button clicked');
+    function init() {
+        $('#input').keypress(function(e) {
+            if (e.which == 13) {
+              var tmp = $('#input').val();
+              var re = new RegExp('/^[^a-zA-Z]+$/');
 
-    $('#submit').on('click', function(){
-      getData();
-    })
-  }
+              if (tmp != '' && tmp === re) {
+                getData();
+                alert('success');
+              } else {
+                alert('Please enter a quote. Only letters are accepted.');
+              }
+            }
+        });
+    }
 
-  init();
 
-  function getData() {
-      var query = $('#input').val();
-      var url = 'https://www.quandl.com/api/v3/datasets/WIKI/' + query + '.json?auth_token=6znG6q5S2VrTnsPkDz3U';
-      $.get(url, function(data) {
-          console.log(data);
-          alert('success')
-      })
-  }
+    init();
+
+    function getData() {
+        var query = $('#input').val();
+        console.log('success');
+        var url = 'https://www.quandl.com/api/v3/datasets/WIKI/' + query + '.json?auth_token=6znG6q5S2VrTnsPkDz3U';
+        $.get(url, function(data) {
+            console.log(data);
+            writeData(data);
+        })
+    }
+
+    function writeData(data) {
+        $('#data').append('<h1>it works</h1>');
+    }
 
 }())
