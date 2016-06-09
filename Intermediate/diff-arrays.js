dc26befb3aeff8049c9631c91d3cbe4e563232cf
Alejandro Gomez <agomez.amdg@gmail.com>
2016-06-08 20:18:00 -0400

    diff arrays

--- Intermediate/diff-arrays.js
+++ Intermediate/diff-arrays.js
@@ -2,9 +2,19 @@
 // Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. In other words, return the symmetric difference of the two arrays.
 
 function diffArray(arr1, arr2) {
-  var newArr = [];
+
+  //Merge arrays
+  var newArr = arr1.concat(arr2);
+
+  //Checks against filter method to see if the element is found in either arr
+  function check(item) {
+    if (arr1.indexOf(item) === -1 || arr2.indexOf(item) === -1) {
+      return item;
+    }
+  }
+
   // Same, same; but different.
-  return newArr;
+  return newArr.filter(check);
 }
 
 diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
